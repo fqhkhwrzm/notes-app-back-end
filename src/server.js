0779b00f6dea2755e5e7dd30944d92d8430829c8
fqhkhrzm <muhammadfaqihk16@gmail.com>
2024-11-09 23:11:32 +0700

    Fix bugs host value

--- src/server.js
+++ src/server.js
@@ -5,6 +5,11 @@ const init = async () => {
     const server = Hapi.server({
         port: 5000,
         host: 'localhost',
+        // Jika di GCLOUD, pake ini:
+        /*
+            host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+            properti host akan bernilai sesuai dengan environment yang ditetapkan.
+        */
         // Menerapkan CROSS ORIGIN RESOURCE SHARING
         // Supaya bisa berinteraksi jika terdapat perbedaan origin web server, sebagai contoh:
         // origin web server kita http://localhost:5000/ dg port 5000, tetapi origin
